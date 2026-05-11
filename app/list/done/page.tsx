ff39043ebb0791ce31fd26fb5686e6edb75c7288
oartuu <artu.donascimento@gmail.com>
2026-05-11 12:51:38 -0300

    done page

--- app/list/done/page.tsx
+++ app/list/done/page.tsx
@@ -1,9 +1,17 @@
+import { Card, CardContent } from '@/components/ui/card';
+import { Item, ItemContent } from '@/components/ui/item'
+import { BookmarkCheck } from 'lucide-react'
 import React from 'react'
 
 export default function page() {
   return (
-    <div>
-      presença salva
+    <div className="h-dvh flex items-center justify-center bg-zinc-950">
+      <Card className='bg-green-300'>
+        <CardContent className='flex flex-col items-center justify-between gap-4'>
+          <h1>Presença Enviada</h1>
+          <BookmarkCheck />
+        </CardContent>
+      </Card>
     </div>
-  )
+  );
 }
